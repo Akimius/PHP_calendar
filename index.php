31486011404808d8046a6f8bd3a3342c9c6c2153
Akim Savchenko Len <akim.savchenko@gmail.com>
2017-07-08 15:10:31 +0300

    time zone changed

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 // Set your timezone!!
-date_default_timezone_set('Ukraine/Kiev'); // Set Ukrainian time
+date_default_timezone_set('Europe/Kiev'); // Set Ukrainian time
 
 // Get prev & next month
 if (isset($_GET['ym'])) {
